46b1ad330aa28bb8e4f68e79236998020898b341
Morteza <morteza.tourani@gmail.com>
2024-12-27 23:14:53 +0300

    feat(oapi-gen): use openapi docker image

--- tools/generator/generate.sh
+++
@@ -1,5 +0,0 @@
-openapi-generator-cli generate \
-    -i http://todo-api:3001/docs-json \
-    --generator-name typescript-axios \
-    -o /local/build \
-    --additional-properties=useSingleRequestParameter=true
