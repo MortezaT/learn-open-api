3bd54254783e6a33be559f472fb9a69078a045e1
Morteza <morteza.tourani@gmail.com>
2024-12-28 19:48:09 +0300

    feat(oapi-gen): less is more

--- apps/client/scripts/api/generate.sh
+++
@@ -1,9 +0,0 @@
-openapi-generator-cli generate \
-    -i http://api/docs-json \
-    -g typescript-axios \
-    -o /local/build \
-    -p=useSingleRequestParameter=true
-
-rm -rf /local/generated/*
-mkdir -p /local/generated
-mv /local/build/**.ts /local/generated
